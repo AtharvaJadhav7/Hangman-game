747d3ae0ed4cb45f103ec0c9063194d9ffe8b3c9
AtharvaJadhav7 <114385610+AtharvaJadhav7@users.noreply.github.com>
2022-11-05 23:04:46 +0530

    Fixed a bug

--- hangman.py
+++ hangman.py
@@ -13,9 +13,9 @@ def valid_word(words):
 
 
 def hangman():
-    word = valid_word(words)
+    word = valid_word(words).upper()
     word_letters = set(word) # letters in word
-    letter = set(string.ascii_uppercase)
+    alphabet = set(string.ascii_uppercase)
     used_letters = set() # letters used by user
 
 
@@ -24,12 +24,12 @@ def hangman():
 
     while len(word_letters)>0 and lives>0:
         print('You have',lives,'lives left and','you have used these letters: ',' '.join(used_letters))
-        word_list=[a if a in used_letters else '_' for a in word]
+        word_list=[letter if letter in used_letters else '_' for letter in word]
         print('Current word: ', ' '.join(word_list))
         
         
         user_letter = input('Guess a letter: ').upper()
-        if user_letter in letter - used_letters:
+        if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
